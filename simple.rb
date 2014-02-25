ec3adc490ccd189051c531ec7ba55bb93099f168
Jason Thigpen <darwin@senet.us>
2014-02-25 09:12:39 -0800

    Change

--- simple.rb
+++ simple.rb
@@ -3,13 +3,13 @@
 set :bind, '0.0.0.0'
 
 get '/' do
-   wait_for = 0.01
+   wait_for = 0.9
    sleep(0.1)
    'Hello world!'
 end
 
 get '/test' do
-  wait_for = 0.1
+  wait_for = 1.1
   sleep(1.0)
   'I was tired'
 end
