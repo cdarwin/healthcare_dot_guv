55c33cccd4ab2088847a33b7603df01790f60878
Jason Thigpen <darwin@senet.us>
2014-02-24 23:22:33 -0800

    Change

--- simple.rb
+++ simple.rb
@@ -3,13 +3,13 @@
 set :bind, '0.0.0.0'
 
 get '/' do
-   wait_for = 0.05
+   wait_for = 0.06
    sleep(rand(0.0..wait_for))
    'Hello world!'
 end
 
 get '/test' do
-  wait_for = 0.7
+  wait_for = 0.8
   sleep(rand(0.0..wait_for))
   'I was tired'
 end
